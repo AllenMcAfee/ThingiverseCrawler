d47f0c2a69b190d399f9e58ae91bde2fb995492a
Allen McAfee <allen@robo3d.com>
2016-08-17 20:19:17 -0700

    Update thingiverse_crawler.py

--- thingiverse_crawler.py
+++ thingiverse_crawler.py
@@ -193,6 +193,7 @@ def save_records(records):
     with open("summary.csv", 'w') as fout:
         fout.write("image_url, thing_id, file_id, license, links\n");
         for entry in records: #entry= (image_url, thing_id, file_id, license, links)
+            print entry
             links = str(entry[4]).replace(',' , ';')
             vals = [entry[0], entry[1], entry[2], entry[3], links]
             row = '{0},{1},{2},{3},{4}\n'.format(*vals)

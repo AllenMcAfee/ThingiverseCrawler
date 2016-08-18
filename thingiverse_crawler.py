c0a5db2cfc81ee7a5c2007162748aaaad1acfece
Allen McAfee <allen@robo3d.com>
2016-08-17 20:21:09 -0700

    Update thingiverse_crawler.py

--- thingiverse_crawler.py
+++ thingiverse_crawler.py
@@ -174,6 +174,7 @@ def get_image_link(image_id):
     url = base_url.format("renders/", image_id);
     r = requests.head(url);
     link = r.headers.get("Location", None);
+    print link 
     if link is not None:
         return link;
 
